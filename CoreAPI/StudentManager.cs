40e7d2ff4447879ae84720093fb5da6cb87b5c34
Alriosa <carlos.alfredo.rios15@gmail.com>
2022-03-29 01:07:35 -0600

    17 AHHHHHHHHH

--- CoreAPI/StudentManager.cs
+++ CoreAPI/StudentManager.cs
@@ -3,10 +3,77 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataAccess.Crud;
+using Entities_POJO;
+using Exceptions;
 
 namespace CoreAPI
 {
     public class StudentManager : BaseManager
     {
+        private StudentCrudFactory crudStudent;
+
+        public StudentManager()
+        {
+            crudStudent = new StudentCrudFactory();
+        }
+
+        public void Create(Student student)
+        {
+            var c = crudStudent.Retrieve<Student>(student);
+            //try
+            //{
+               
+
+            //    if (c != null)
+            //    {
+            //        //Student already exist
+            //        throw new BussinessException(3);
+            //    }
+
+            //    if (student.Age >= 18)
+            //        crudStudent.Create(student);
+            //    else
+            //        throw new BussinessException(2);
+            //}
+            //catch (Exception ex)
+            //{
+            //    ExceptionManager.GetInstance().Process(ex);
+            //}
+        }
+
+        public List<Student> RetrieveAll()
+        {
+            return crudStudent.RetrieveAll<Student>();
+        }
+
+        public Student RetrieveById(Student student)
+        {
+            Student c = null;
+            try
+            {
+                c = crudStudent.Retrieve<Student>(student);
+                if (c == null)
+                {
+                    throw new BussinessException(4);
+                }
+            }
+            catch (Exception ex)
+            {
+                ExceptionManager.GetInstance().Process(ex);
+            }
+
+            return c;
+        }
+
+        public void Update(Student student)
+        {
+            crudStudent.Update(student);
+        }
+
+        public void Delete(Student student)
+        {
+            crudStudent.Delete(student);
+        }
     }
 }
