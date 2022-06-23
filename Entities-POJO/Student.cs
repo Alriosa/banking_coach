e0c71d5e9d93055681aa16ac798038b0a58b89c2
Mariela <mbonilla.guti@gmail.com>
2022-06-23 00:16:40 -0600

    68

--- Entities-POJO/Student.cs
+++ Entities-POJO/Student.cs
@@ -11,68 +11,101 @@ namespace Entities_POJO
         public int StudentID { get; set; }
         public string BankingStudent { get; set; }
         public string UserActiveStatus { get; set; }
-        public DateTime EntryDate { get; set; }
-        public string FirstName { get; set; }
-        public string SecondName { get; set; }
-        public string LastName { get; set; }
-        public string SecondLastName { get; set; }
+        public string CompleteName { get; set; }
         public string IdType { get; set; }
         public string IdentificationNumber { get; set; }
         public DateTime Birthdate { get; set; }
-        public string Gender { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
         public string Province { get; set; }
         public string NProvince { get; set; }
         public string Canton { get; set; }
         public string NCanton { get; set; }
         public string District { get; set; }
         public string NDistrict { get; set; }
+        public string LaboralStatus { get; set; }
+        public string Workstation {get; set;}
+        public string Experience { get; set; }
+        public string LaboralExperience { get; set; }
+        public string EducationStatus { get; set; }
+        public string InstitutionAcademicType { get; set; }
+        public string InstitutionAcademicName { get; set; }
+        public string AcademicPreparation { get; set; }
+        public string UniversityLevel { get; set; }
+        public string UniversityDegree { get; set; }
+        public string ExcelLevel { get; set; }
+        public string JobAvailability { get; set; }
+        public string TransportAvailability { get; set; }
+        public string Vehicle { get; set; }
+        public string DriverLicenses { get; set; }
+        public string CoursesBankingCoach { get; set; }
+        public string TeachersBankingCoach { get; set; }
+        public string Language { get; set; }
+        public DateTime CourseDateFinish { get; set; }
+        public string CourseDay { get; set; }
+        public string CourseSchedule { get; set; }
+        public string CoursePlace { get; set; }
+        public string BankingCoachCertificate { get; set; }
+        public string Curriculum { get; set; }
+        public string AgreeJobExchange { get; set; }
         public string UserType { get; set; }
         public string StudentLogin { get; set; }
         public string StudentPassword { get; set; }
-        public string LaboralStatus { get; set; }
-        public string WorkAddress { get; set; }
-        public string Email { get; set; }
-        public string PrimaryPhone { get; set; }
-        public string SecondaryPhone { get; set; }
-        public string LaboralExperience { get; set; }
-
         public string UserLogin { get; set; }
         public Student()
         {
             
         }
-        public Student(int studentId, string bankingStudent, string userActiveStatus, DateTime entryDate, string firstName, string secondName, string lastName, string secondLastName, string idType, string identificationNumber, DateTime birthdate, string gender, string canton, string district, string userType, string studentLogin, string studentPassword, string laboralStatus, string workAddress, string email, string laboralExperience, string primaryPhone, string secondaryPhone, string province, string nProvince, string nCanton, string nDistrict)
+
+        public Student(int studentID, string bankingStudent, string userActiveStatus, string completeName, string idType, string identificationNumber, DateTime birthdate, int age, string email, string phoneNumber, string province, string nProvince, string canton, string nCanton, string district, string nDistrict, string laboralStatus, string workstation, string experience, string laboralExperience, string studentStatus, string institutionAcademicType, string institutionAcademicName, string academicPreparation, string universityLevel, string universityDegree, string excelLevel, string jobAvailability, string transportAvailability, string vehicle, List<string> driverLicense, List<string> coursesBankingCoach, List<string> teachersBankingCoach, string language, DateTime courseDateFinish, string courseDay, string courseSchedule, string coursePlace, string bankingCoachCertificate, string curriculum, string agreeJobExchange, string userType, string studentLogin, string studentPassword, string userLogin)
         {
-            StudentID = studentId;
+            StudentID = studentID;
             BankingStudent = bankingStudent;
             UserActiveStatus = userActiveStatus;
-            EntryDate = entryDate;
-            FirstName = firstName;
-            SecondName = secondName;
-            LastName = lastName;
-            SecondLastName = secondLastName;
+            CompleteName = completeName;
             IdType = idType;
             IdentificationNumber = identificationNumber;
             Birthdate = birthdate;
-            Gender = gender;
+            Age = age;
+            Email = email;
+            PhoneNumber = phoneNumber;
             Province = province;
+            NProvince = nProvince;
             Canton = canton;
+            NCanton = nCanton;
             District = district;
+            NDistrict = nDistrict;
+            LaboralStatus = laboralStatus;
+            Workstation = workstation;
+            Experience = experience;
+            LaboralExperience = laboralExperience;
+            StudentStatus = studentStatus;
+            InstitutionAcademicType = institutionAcademicType;
+            InstitutionAcademicName = institutionAcademicName;
+            AcademicPreparation = academicPreparation;
+            UniversityLevel = universityLevel;
+            UniversityDegree = universityDegree;
+            ExcelLevel = excelLevel;
+            JobAvailability = jobAvailability;
+            TransportAvailability = transportAvailability;
+            Vehicle = vehicle;
+            DriverLicense = driverLicense;
+            CoursesBankingCoach = coursesBankingCoach;
+            TeachersBankingCoach = teachersBankingCoach;
+            Language = language;
+            CourseDateFinish = courseDateFinish;
+            CourseDay = courseDay;
+            CourseSchedule = courseSchedule;
+            CoursePlace = coursePlace;
+            BankingCoachCertificate = bankingCoachCertificate;
+            Curriculum = curriculum;
+            AgreeJobExchange = agreeJobExchange;
             UserType = userType;
             StudentLogin = studentLogin;
             StudentPassword = studentPassword;
-            LaboralStatus = laboralStatus;
-            WorkAddress = workAddress;
-            Email = email;
-            LaboralExperience = laboralExperience;
-            PrimaryPhone = primaryPhone;
-            SecondaryPhone = secondaryPhone;
-            NProvince = nProvince;
-            NCanton = nCanton;
-            NDistrict = nDistrict;
+            UserLogin = userLogin;
         }
-
-       
     }
     
 }
