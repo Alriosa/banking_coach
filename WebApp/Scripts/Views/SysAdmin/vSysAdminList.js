854abc91dff7aecdf76e10d20b5ab4950744044e
Mariela34 <mbonilla.guti@gmail.com>
2023-07-14 00:54:24 -0600

    add title in btn to edit in admin list

--- WebApp/Scripts/Views/SysAdmin/vSysAdminList.js
+++ WebApp/Scripts/Views/SysAdmin/vSysAdminList.js
@@ -120,7 +120,8 @@ async function changeStatusAdmin(data, data2, button) {
 		var index = t.row($(button).parents('tr')).index();
 		var data = t.row($(button).parents('tr')).data();
 		data[4] = (adminData.UserActiveStatus == '1') ? 'Activo' : 'Inactivo'
-		let btnActions = '<button class="btn btn-primary" style="margin-right: 10px;" onclick="updateAdmin(' + adminData.SysAdminUserID + ')" id="updateAdmin"><i class="fa fa-user-edit"></i></button ><button ' + (data[4] == 'Activo' ? 'class="btn btn-danger"' : 'class="btn btn-success"') + ' id="changeStatus' + adminData.SysAdminUserID + '"  onclick="changeStatusAdmin(' + adminData.SysAdminUserID + ',\'' + data[4] + '\', this)">' + (data[4] == 'Activo' ? '<i class="fa fa-eye-slash"></i>' : '<i class="fa fa-eye"></i>') + '</button >';
+		let btnActions = '<button class="btn btn-primary"  style="margin-right: 10px;" title="Actualizar Administrador" onclick="updateAdmin(' + adminData.SysAdminUserID + ')" id="updateAdmin"><i class="fa fa-user-edit"></i></button ><button ' + (data[4] == 'Activo' ? 'class="btn btn-danger"' : 'class="btn btn-success"') + ' id="changeStatus' + adminData.SysAdminUserID + '" title="' + (data[i].UserActiveStatus == 'Activo' ? 'Desactivar Admin' : 'Activar Admin') + '" onclick="changeStatusAdmin(' + adminData.SysAdminUserID + ',\'' + data[4] + '\', this)">' + (data[4] == 'Activo' ? '<i class="fa fa-eye-slash"></i>' : '<i class="fa fa-eye"></i>') + '</button >';
+
 		data[5] = btnActions
 
 
